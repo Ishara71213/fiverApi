7606b8cb8f9bff050a642bf2739509b26a53d312
Madushanka Ishara <madushankaishara48@gmail.com>
2023-04-03 11:46:27 +0530

    edit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const login = async (req, res, next) => {
     const { password, ...info } = user._doc;
     res
       .cookie("accessToken", token, {
-        httpOnly: true,
+        // httpOnly: true,
         sameSite: "none",
         secure: "true",
       })
